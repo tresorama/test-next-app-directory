6cc55edfc8226a6affd78a5f00e0f69e479e9ecf
Jacopo Marrone <jacopo.marrone27@gmail.com>
2024-07-20 17:07:52 +0000

    feat(css): better file hint ui

--- src/ui/FileHint.tsx
+++ src/ui/FileHint.tsx
@@ -1,1 +1,5 @@
-export const FileHint = ({ fileName }: { fileName: string; }) => <code className="font-light text-xs opacity-40">{fileName}</code>;
+export const FileHint = ({ fileName }: { fileName: string }) => (
+  <code className="[word-break:break-word] font-light text-xs opacity-40">
+    {fileName}
+  </code>
+);
